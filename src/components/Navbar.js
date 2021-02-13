2614ff8073f664a3008c03a7a1d59309c32bb462
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-02-13 19:13:51 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+function Navbar(){
+    return(
+        <nav>
+            <ul>
+                <li><a href="#geeky">Being Geeky</a></li>
+                <li><a href="#">Tutorial</a></li>
+                <li><a href="#">Vedio</a></li>
+                <li><a href="#">Cupons</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
