9ee3d30367f3880baa12508a9f844a4f852fca51
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-02-14 23:06:35 +0530

    cupons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,8 @@ function Navbar(){
             <ul>
                 <li><a href="#geeky">Being Geeky</a></li>
                 <li><a href="#">Tutorial</a></li>
-                <li><a href="#">Vedio</a></li>
-                <li><a href="#">Cupons</a></li>
+                <li><a href="#vedio">Vedio</a></li>
+                <li><a href="#cu">Cupons</a></li>
             </ul>
         </nav>
     );
