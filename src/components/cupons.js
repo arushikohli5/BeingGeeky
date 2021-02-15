59e421029d5d76da286937d6c29335eaded9e3f8
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-02-15 23:13:30 +0530

    cupons

--- src/components/cupons.js
+++ src/components/cupons.js
@@ -3,7 +3,19 @@ import React from 'react';
 function Cupons(){
     return (
         <div id="cu" className="cu">
-        
+            <div className="photo">
+                <img src="https://beinggeeky-hnip75cdkgvvk6vt.web.codequotient.com/public/coupon.jpg" alt="cupons"/>
+            </div>
+            <div className="cumain">
+                <h2>Coupons</h2>
+                <p> You can apply different coupons to get extra discount on selected courses.Certain internet providers exploit the fact that fill text cannot be recognized by automatic search engines - meaningful information cannot be distinguished from meaningless: Target-generated dummy text mixed with a certain combination of search terms can lead to an increased frequency of visits by search machine users. As a consequence, advertising revenues, which rely on website 'hits', are increased.
+                </p>
+                <p>Already have Coupon?</p>
+                <hr />
+                 <a href="#" class="text-secondary">
+                  <i class="fas fa-chevron-right"></i> Redeem 
+                  </a>
+            </div>
         </div>
     );
 }
