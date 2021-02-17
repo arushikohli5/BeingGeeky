efe63d87a703aa08c39a0dddc1fe45d4367c2cda
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-02-17 23:18:07 +0530

    footer

---
+++ src/components/footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <div className="fo">
+            <ul>
+                <li className="first">Address</li>
+                <li>ZZZZ Workspace</li>
+                <li>Street 43,building 4A</li>
+                <li>Industrial Area</li>
+                <li>Aqwertyu </li>
+                <li>Mumbai</li>
+            </ul>
+            <ul>
+                <li className="first">Clients</li>
+                <li>XYZ org</li>
+                <li>ABC org</li>
+                <li>IJK org</li>
+                <li>Qwerty Corp</li>
+                <li>ZAQ PVT</li>
+            </ul>
+            </div>
+    )
+}
